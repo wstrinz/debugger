b7d7b29007bce6211433bdb1c0cc18b826657179
Anton Astashov <anton.astashov@gmail.com>
2012-11-22 23:41:48 -0800

    Add tests for 'conditions' command

---
+++ test/new/conditions_test.rb
@@ -0,0 +1,67 @@
+require_relative 'test_helper'
+
+describe "Conditions" do
+  include TestDsl
+
+  describe "setting condition" do
+    before { enter 'break 3' }
+
+    describe "successfully" do
+      before { enter ->{"cond #{Debugger.breakpoints.first.id} b == 5"}, "cont" }
+      it "must stop at the breakpoint if condition is true" do
+        debug_file('conditions') { state.line.must_equal 3 }
+      end
+
+      it "must assign that expression to breakpoint" do
+        debug_file('conditions') { Debugger.breakpoints.first.expr.must_equal "b == 5" }
+      end
+    end
+
+    it "must not stop at the breakpoint if condition is false" do
+      enter "break 4", ->{"cond #{Debugger.breakpoints.first.id} b == 3"}, "cont"
+      debug_file('conditions') { state.line.must_equal 4 }
+    end
+
+    it "must ignore the condition if its syntax is incorrect" do
+      enter "break 3", ->{"cond #{Debugger.breakpoints.first.id} b =="}, "break 4", "cont"
+      debug_file('conditions') { state.line.must_equal 4 }
+    end
+
+    it "must assign the expression to the breakpoint anyway, even if its syntax is incorrect" do
+      enter "break 3", ->{"cond #{Debugger.breakpoints.first.id} b =="}, "break 4", "cont"
+      debug_file('conditions') { Debugger.breakpoints.first.expr.must_equal "b ==" }
+    end
+
+    it "must work with full command name too" do
+      enter ->{"condition #{Debugger.breakpoints.first.id} b == 5"}, "cont"
+      debug_file('conditions') { state.line.must_equal 3 }
+    end
+  end
+
+
+  describe "removing conditions" do
+    before { enter "break 3 if b == 3", "break 4", ->{"cond #{Debugger.breakpoints.first.id}"}, "cont" }
+
+    it "must remove the condition from the breakpoint" do
+      debug_file('conditions') { Debugger.breakpoints.first.expr.must_be_nil }
+    end
+
+    it "must not stop on the breakpoint" do
+      debug_file('conditions') { state.line.must_equal 3 }
+    end
+  end
+
+
+  describe "errors" do
+    it "must show error if there are no breakpoints" do
+      enter 'cond 1 true'
+      debug_file('conditions')
+      check_output "No breakpoints have been set."
+    end
+
+    it "must not set breakpoint condition if breakpoint id is incorrect" do
+      enter 'break 3', 'cond 8 b == 3', 'cont'
+      debug_file('conditions') { state.line.must_equal 3 }
+    end
+  end
+end
