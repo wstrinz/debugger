ec0958b5d239d66019b729edb771a211778e56e1
Anton Astashov <anton.astashov@gmail.com>
2012-12-06 00:14:21 -0800

    Add tests for the 'thread' command
    Totally not satisfied by the test suite for 'thread', sometimes it fails
    with Segmentation Fault, I added 'puts' to avoid weird race conditions
    (which is very wrong, I know).
    
    Will need to revisit this in the future, probably.

---
+++ test/new/examples/thread.rb
@@ -0,0 +1,28 @@
+debugger
+class ThreadExample
+  def initialize
+    Thread.main[:should_break] = false
+  end
+
+  def launch
+    t1 = Thread.new do
+      while true
+        break if Thread.main[:should_break]
+        sleep 0.02
+      end
+    end
+
+    t2 = Thread.new do
+      while true
+        sleep 0.02
+      end
+    end
+
+    t1.join
+    Thread.main[:should_break]
+  end
+end
+
+t = ThreadExample.new
+t.launch
+t
