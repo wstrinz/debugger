e2b90c8876f9998670dbf43d757e63b319a86476
Gabriel Horner <gabriel.horner@gmail.com>
2013-01-28 02:30:45 -0500

    remove unnecessary requires and $LOAD_PATH manipulation

--- test/new/test_helper.rb
+++ test/new/test_helper.rb
@@ -1,11 +1,6 @@
 require 'pathname'
-require 'rubygems'
-require 'bundler/setup'
 require 'minitest/autorun'
 require 'mocha/setup'
-
-lib_path = (Pathname.new(__FILE__) + "../../../lib").cleanpath
-$LOAD_PATH.unshift(lib_path)
 
 require 'debugger'
 Dir.glob(File.expand_path("../support/*.rb", __FILE__)).each { |f| require f }
