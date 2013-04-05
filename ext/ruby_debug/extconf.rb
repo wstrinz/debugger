fa42ec84855d567ae000b8487827586a0d3d20d1
Gabriel Horner <gabriel.horner@gmail.com>
2013-04-05 09:53:12 -0400

    bring back 1.9 support in extconf while keeping 2.x minimal

--- ext/ruby_debug/extconf.rb
+++ ext/ruby_debug/extconf.rb
@@ -1,10 +1,84 @@
-if RUBY_VERSION < "2.0"
+if RUBY_VERSION < "1.9"
   STDERR.print("Ruby version is too old\n")
   exit(1)
 end
 
-require 'mkmf'
-# $optflags = '-O0'
-CONFIG['optflags'] = '-O0'
+if RUBY_VERSION < '2.0.0'
+  # autodetect ruby headers
+  unless ARGV.any? {|arg| arg.include?('--with-ruby-include') }
+    require 'rbconfig'
+    bindir = RbConfig::CONFIG['bindir']
+    if bindir =~ %r{(^.*/\.rbenv/versions)/([^/]+)/bin$}
+      ruby_include = "#{$1}/#{$2}/include/ruby-#{RUBY_VERSION.start_with?("1.9") ? "1.9.1" : RUBY_VERSION}/ruby-#{$2}"
+      ARGV << "--with-ruby-include=#{ruby_include}"
+    elsif bindir =~ %r{(^.*/\.rvm/rubies)/([^/]+)/bin$}
+      ruby_include = "#{$1}/#{$2}/include/ruby-#{RUBY_VERSION.start_with?("1.9") ? "1.9.1" : RUBY_VERSION}/#{$2}"
+      ruby_include = "#{ENV['rvm_path']}/src/#{$2}" unless File.exist?(ruby_include)
+      ARGV << "--with-ruby-include=#{ruby_include}"
+    end
+  end
+end
 
-create_makefile('ruby_debug')
+require "mkmf"
+begin
+  require "debugger/ruby_core_source"
+rescue LoadError
+  warn '*** RUBY_CORE_SOURCE NOT INSTALLED **'
+end
+
+hdrs = if RUBY_VERSION == '1.9.2'
+  lambda {
+    have_struct_member("rb_method_entry_t", "body", "method.h")
+    have_header("vm_core.h") and have_header("iseq.h") and have_header("insns.inc") and
+    have_header("insns_info.inc") and have_header("eval_intern.h")
+  }
+elsif RUBY_VERSION == '1.9.3'
+  lambda {
+    iseqs = %w[vm_core.h iseq.h]
+    begin
+      have_struct_member("rb_method_entry_t", "called_id", "method.h") or
+      have_struct_member("rb_control_frame_t", "method_id", "method.h")
+    end and
+    have_header("vm_core.h") and have_header("iseq.h") and have_header("insns.inc") and
+    have_header("insns_info.inc") and have_header("eval_intern.h") or return(false)
+    have_type("struct iseq_line_info_entry", iseqs) or
+    have_type("struct iseq_insn_info_entry", iseqs) or
+    return(false)
+    if checking_for(checking_message("if rb_iseq_compile_with_option was added an argument filepath")) do
+        try_compile(<<SRC)
+#include <ruby.h>
+#include "vm_core.h"
+extern VALUE rb_iseq_new_main(NODE *node, VALUE filename, VALUE filepath);
+SRC
+      end
+      $defs << '-DRB_ISEQ_COMPILE_5ARGS'
+    end
+  }
+end
+
+## Create Makefile
+if RUBY_VERSION < '2.0.0'
+  require 'fileutils'
+
+  header_dir = RUBY_VERSION.gsub('.', '')
+  current_dir = File.dirname(__FILE__)
+  %w{ruby_debug.h ruby_debug.c breakpoint.c}.each do |file|
+    FileUtils.cp("#{current_dir}/#{header_dir}/#{file}", current_dir)
+  end
+
+  dir_config("ruby")
+  if !Debugger::RubyCoreSource.create_makefile_with_core(hdrs, "ruby_debug")
+    STDERR.print("Makefile creation failed\n")
+    STDERR.print("*************************************************************\n\n")
+    STDERR.print("  NOTE: If your headers were not found, try passing\n")
+    STDERR.print("        --with-ruby-include=PATH_TO_HEADERS      \n\n")
+    STDERR.print("*************************************************************\n\n")
+    exit(1)
+  end
+
+else
+  # $optflags = '-O0'
+  CONFIG['optflags'] = '-O0'
+
+  create_makefile('ruby_debug')
+end
